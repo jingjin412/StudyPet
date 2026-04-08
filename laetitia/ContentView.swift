14b6a16b581c313a9a79b45384244df9f8240f45
jingjin412 <b10204022@g.ntu.edu.tw>
2026-04-08 19:20:36 +0800

    Initial Commit

---
+++ laetitia/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  laetitia
+//
+//  Created by Jing on 2026/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ laetitia/laetitiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  laetitiaApp.swift
+//  laetitia
+//
+//  Created by Jing on 2026/4/8.
+//
+
+import SwiftUI
+
+@main
+struct laetitiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
